9cb50cc3fda3fd88ee8fc0991eee6b5ae485d488
Obua Stella <stellaobua123@gmail.com>
2024-01-06 23:45:33 +0100

    Installed dependency for customizer component

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,3 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { AninmatePresence, motion } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import config from "../config/config"; // will be used later on to set the url of our state
+import state from "../store";
+import { downlaod } from "../assets";
+import { downloadCanvasToImage, reader } from "../config/helpers";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
+import { fadeAnimation, slideAnimation } from "../config/motion";
 export const Customizer = () => {
+  // This will be the main part of out application
   return <div>Customizer</div>;
 };
